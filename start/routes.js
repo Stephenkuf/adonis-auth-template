7940d352b46f75a3ee477b65e6b186a9869dd2fc
Stephen Kuforiji <stephenkuforijif@gmail.com>
2021-09-17 12:19:26 +0100

    adonis auth scaffold

--- start/routes.js
+++ start/routes.js
@@ -20,47 +20,8 @@ require('./apiAuthRoutes.js');
 const Route = use('Route')
 
 Route.get('/', () => {
-    return { greeting: 'Hello world in JSON' }
+    return { greeting: 'Welcome to APIs by Flitrs' }
 })
 
 // metadata routes
 Route.get('/getMetadata', 'Metadata/MetadatumController.getMetadata');
-Route.get('/signupTeamList', 'Metadata/MetadatumController.getSignUpTeamList')
-Route.get('/playersList', 'Metadata/MetadatumController.getAllPlayers')
-Route.get('/weekFixtures', 'Metadata/MetadatumController.getWeekFixtures')
-
-// team selection routes , select team members , remove team members , add team member(s) , view User team  , 
-Route.post('/createTeam', 'TeamCreation/TeamManagementController.createTeam').middleware(['auth'])
-Route.put('/editTeam/:team_id', 'TeamCreation/TeamManagementController.editTeam').middleware(['auth'])
-Route.get('/viewUserTeam/:team_id', 'TeamCreation/TeamManagementController.viewUserTeam').middleware(['auth'])
-Route.get('/viewUserProfile', 'TeamCreation/TeamManagementController.viewUserProfile').middleware(['auth'])
-
-Route.get('/updateRankings', 'Metadata/MetadatumController.updateRanking').middleware(['auth'])
-Route.get('/viewSquadRankings', 'Ranking/RankingController.viewSquadRankings').middleware(['auth'])
-Route.get('/userTeamRanking/:team_id', 'Ranking/RankingController.userTeamRankings').middleware(['auth'])
-
-Route.get("/test", "SystemLeague/SystemLeagueController.test")
-Route.get("/countries", "SystemLeague/SystemLeagueController.getSystemCountries")
-Route.get("/leaguesByCountryName/:country_name", "SystemLeague/SystemLeagueController.getLeagueByCountryName")
-Route.get("/getAllSystemLeagues", "SystemLeague/SystemLeagueController.getAllSystemLeagues")
-Route.get("/getLeagueFixturesBetweenTwoDates/:league/:season/:from/:to", "SystemLeague/SystemLeagueController.getLeagueFixturesBetweenTwoDates")
-
-
-//League Creation
-Route.post("/createLeague", "League/LeagueCreationController.createLeague").validator("LeagueCreation").middleware(['auth'])
-Route.put("/leagueSettings", "League/LeagueCreationController.leagueSettings").validator("LeagueSettings").middleware(['auth'])
-// Route.get("/leagueWeeks", "League/LeagueCreationController.leagueWeeks").middleware(['auth'])
-Route.get("/leagueParticipantRanking/:league_id", "League/LeagueCreationController.leagueParticipantRanking").middleware(['auth'])
-
-//Join League With Code
-Route.post("/joinLeagueWithcode", "League/LeagueSettingController.joinLeagueWithCode").validator("joinLeagueWithCode").middleware(['auth'])
-
-//Join League
-Route.get("/joinLeague/:league_id/:team_id", "League/LeagueSettingController.joinLeague").middleware(['auth'])
-
-//Leave League
-Route.get("/leaveLeague/:league_id/:team_id", "League/LeagueSettingController.leaveLeague").middleware(['auth'])
-
-//Leave Comment
-Route.post("/leagueComment", "League/LeagueSettingController.leagueComment").validator("leagueComment").middleware(['auth'])
-Route.get("/leagueComment/:league_id", "League/LeagueSettingController.getLeagueComment").middleware(['auth'])
